4ea5e8e62da1c9b130e27a8eece17b73774e22d1
Gelzieny R. Martins <gelzieny@gmail.com>
2025-01-13 17:36:29 -0300

    Finalizando Interface da LP e pg internas

--- app/components/landing-page/header/index.tsx
+++ app/components/landing-page/header/index.tsx
@@ -1,35 +1,15 @@
-export function Hero() {
-  return (
-    <div className="flex ">
-      <div className="w-full flex flex-col gap-2 mt-[35vh] ">
-        <h1 className="text-5xl font-bold text-white leading-[64px]">
-          Seus projetos e redes sociais em um único link
-        </h1>
-        <h2 className="text-xl leading-6">
-          Crie sua própria página de projetos e compartilhe eles com o mundo.
-          <br />
-          Acompanhe o engajamento com Analytics de cliques
-        </h2>
-        <div className="flex items-center gap-2 w-full -mt-[10vh]">
-          <span className="text-white text-xl">projectinbio.com</span>
-          <input type="text" />
-          <button type="submit">Criar agora</button>
-        </div>
-      </div>
+import Button from '@/components/ui/button'
 
-      <div className="w-full flex items-center justify-center bg-[radial-gradient(circle_at_50%_50%,#4B2DBB, transparent_55%)]">
-        <div className="relative">
-          {/* <UserCard /> */}
-          <div className="absolute -bottom-[7%] -right-[45%]">
-            {/* <TotalVisits /> */}
-          </div>
-          <div className="absolute top-[20%] -left-[45%] -z-10">
-            {/* <ProjectCard /> */}
-          </div>
-          <div className="absolute -top-[5%] -left-[55%] -z-10">
-            {/* <ProjectCard /> */}
-          </div>
-        </div>
+export function Header() {
+  return (
+    <div className="absolute top-0 left-0 right-0 max-w-7xl mx-auto flex items-center justify-between py-10">
+      <div className="flex items-center gap-4">
+        <img src="/logo.svg" alt="ProjectInBio Logo" />
+        <h3 className="text-white text-2xl font-bold">ProjectInBio</h3>
+      </div>
+      <div className="flex items-center gap-4">
+        <Button>Minha Página</Button>
+        <Button>Sair</Button>
       </div>
     </div>
   )
---
+++ app/components/landing-page/hero/index.tsx
@@ -0,0 +1,42 @@
+import { TextInput } from '@/components/ui/text-input'
+import Button from '@/components/ui/button'
+import { UserCard } from '@/components/commons/user-card'
+import { TotalVisits } from '@/components/commons/total-visits'
+import { ProjectCard } from '@/components/commons/project-card'
+
+export function Hero() {
+  return (
+    <div className="flex ">
+      <div className="w-full flex flex-col gap-2 mt-[35vh] mr-5">
+        <h1 className="text-5xl font-bold text-white leading-[64px]">
+          Seus projetos e redes sociais em um único link
+        </h1>
+        <h2 className="text-xl leading-6">
+          Crie sua própria página de projetos e compartilhe eles com o mundo.
+          <br />
+          Acompanhe o engajamento com Analytics de cliques
+        </h2>
+        <div className="flex items-center gap-2 w-full mt-[10vh]">
+          <span className="text-white text-xl">projectinbio.com/</span>
+          <TextInput placeholder="Seu link" />
+          <Button>Criar agora</Button>
+        </div>
+      </div>
+
+      <div className="w-full flex items-center justify-center bg-[radial-gradient(circle_at_50%_60%,#4B2DBB,transparent_55%)]">
+        <div className="relative top-[20%]">
+          <UserCard />
+          <div className="absolute -bottom-[7%] -right-[45%]">
+            <TotalVisits />
+          </div>
+          <div className="absolute top-[20%] -left-[45%] -z-10">
+            <ProjectCard />
+          </div>
+          <div className="absolute -top-[5%] -left-[55%] -z-10">
+            <ProjectCard />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
